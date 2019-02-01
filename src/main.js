015ae31c6c5617fa2a0899c1cf9b0aaa549d410b
John Lace <lace.john@gmail.com>
2019-02-01 15:15:44 +0000

    Fixed lint issue of "use strict" in main.js.

--- src/main.js
+++ src/main.js
@@ -4,31 +4,35 @@ var React = require('react');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
 
-var App = React.createClass({
-    render: function() {
-        var Child;
+(function(win) {
+    "use strict";
 
-        switch (this.props.route) {
-            case 'about':
-                Child = About;
-                break;
+    var App = React.createClass({
+        render: function() {
+            var Child;
 
-            default:
-                Child = Home;
+            switch (this.props.route) {
+                case 'about':
+                    Child = About;
+                    break;
+
+                default:
+                    Child = Home;
+            }
+
+            return (
+                <div>
+                    <Child />
+                </div>
+            );
         }
+    });
 
-        return (
-            <div>
-                <Child />
-            </div>
-        );
+    function render() {
+        var route = window.location.hash.substr(1);
+        React.render(<App route={route} />, document.getElementById('app'));
     }
-});
 
-function render() {
-    var route = window.location.hash.substr(1);
-    React.render(<App route={route} />, document.getElementById('app'));
-}
-
-window.addEventListener('hashchange', render);
-render();
+    window.addEventListener('hashchange', render);
+    render();
+})(window);
